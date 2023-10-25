698a41fbcc4361066762ed2368b55cfcb49ff7c4
bloodywd <bloodyforreal@gmail.com>
2023-10-25 07:23:19 +0300

    added -f flag

--- project/scripts/gendiff.py
+++ project/scripts/gendiff.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 import argparse
+parser = argparse.ArgumentParser(
+    description="Compares two configuration files and shows a difference."
+)
+parser.add_argument("first_file", type=str)
+parser.add_argument("second_file", type=str)
+parser.add_argument(
+    '-f', '--format', help='set format of output'
+)
+args = parser.parse_args()
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference."
-    )
-    parser.add_argument("first_file", type=str)
-    parser.add_argument("second_file", type=str)
-    args = parser.parse_args()
-    print(args)
     print('hello')
+    print(args.format)
 
 
 if __name__ == '__main__':
