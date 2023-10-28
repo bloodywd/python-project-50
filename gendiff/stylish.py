0b0940a2cea20163bcc3e9b212d091e6e490d595
bloodywd <bloodyforreal@gmail.com>
2023-10-28 15:15:36 +0300

    update stylish

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -12,49 +12,39 @@ def check_nested(key, value, depth):
     if isinstance(value, dict):
         answer += f'{key}: {{\n'
         for k, val in value.items():
-            answer += f'{" " * (depth + 4)}{check_nested(k, val, depth + 4)}'
-        answer += f'{" " * (depth)}}}\n'
+            answer += f'{" " * (depth + 4)}{check_nested(k, val, depth + 4)}\n'
+        answer += f'{" " * (depth)}}}'
         return answer
     else:
-        return f'{key}: {check_type(value)}\n'
+        return f'{key}: {check_type(value)}'
 
 
-def add_similar(key, level, depth):
-    answer = ''
+def add_similar(key, level, depth, answer):
     key_to_string = check_nested(key, level[key], depth)
-    answer += f'{" " * depth}{key_to_string}'
-    return answer
+    answer.append(f'{" " * depth}{key_to_string}')
 
 
-def add_first_only(key, level, depth):
-    answer = ''
+def add_first_only(key, level, depth, answer):
     key_to_string = check_nested(key, level[key], depth)
-    answer += f'{" " * (depth - 2)}- {key_to_string}'
-    return answer
+    answer.append(f'{" " * (depth - 2)}- {key_to_string}')
 
 
-def add_second_only(key, level, depth):
-    answer = ''
+def add_second_only(key, level, depth, answer):
     key_to_string = check_nested(key, level[key], depth)
-    answer += f'{" " * (depth - 2)}+ {key_to_string}'
-    return answer
+    answer.append(f'{" " * (depth - 2)}+ {key_to_string}')
 
 
-def add_diff_values(key, level, depth):
-    answer = ''
+def add_diff_values(key, level, depth, answer):
     key_to_string1 = check_nested(key, level[key][0], depth)
     key_to_string2 = check_nested(key, level[key][1], depth)
-    answer += f'{" " * (depth - 2)}- {key_to_string1}'
-    answer += f'{" " * (depth - 2)}+ {key_to_string2}'
-    return answer
+    answer.append(f'{" " * (depth - 2)}- {key_to_string1}')
+    answer.append(f'{" " * (depth - 2)}+ {key_to_string2}')
 
 
-def add_children(key, level, depth):
-    answer = ''
-    answer += f'{" " * (depth)}{key}: {{\n'
-    answer += lower_level(level[key], depth + 4)
-    answer += f'{" " * (depth)}}}\n'
-    return answer
+def add_children(key, level, depth, answer):
+    answer.append(f'{" " * (depth)}{key}: {{')
+    lower_level(level[key], depth + 4, answer)
+    answer.append(f'{" " * (depth)}}}')
 
 
 FUNCS = {
@@ -66,18 +56,16 @@ FUNCS = {
 }
 
 
-def lower_level(level, depth):
-    level_answer = ''
+def lower_level(level, depth, answer):
     for key in sorted(level.keys()):
         for names in FUNCS:
             if key in level[names]:
-                level_answer += FUNCS[names](key, level, depth)
-    return level_answer
+                FUNCS[names](key, level, depth, answer)
 
 
 def stylish(difference):
-    answer = '{\n'
+    answer = ['{',]
     depth = 0
-    answer += lower_level(difference, depth + 4)
-    answer += '}'
-    return answer
+    lower_level(difference, depth + 4, answer)
+    answer.append('}')
+    return '\n'.join(answer)
