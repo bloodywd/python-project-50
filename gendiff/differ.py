830c13b2a877aafba9ab6bea98f4407c2da2fee7
bloodywd <bloodyforreal@gmail.com>
2023-11-06 15:30:13 +0300

    fixing error

--- gendiff/differ.py
+++ gendiff/differ.py
@@ -4,7 +4,7 @@ from gendiff.formatters.plain import plain
 from gendiff.formatters.format_to_json import json_formatter
 
 
-def generate_diff(file_path1, file_path2, formatter):
+def generate_diff(file_path1, file_path2, formatter='stylish'):
     file1 = open_file(file_path1)
     file2 = open_file(file_path2)
     diff = parse(file1, file2)
