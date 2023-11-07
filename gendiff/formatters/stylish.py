ead89f7cbc4d70975296132271b49fdae0a08b5c
bloodywd <bloodyforreal@gmail.com>
2023-11-07 14:39:07 +0300

    last*3 changes

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -12,14 +12,15 @@ def stringify(data, indent_count):
     for key, value in data.items():
         child = stringify(value, indent_count + 4)
         lines.append(f'{(indent_count + 4) * " "}{key}: {child}')
-    result = itertools.chain("{", lines, [indent_count * " " + "}"])
+    result = itertools.chain("{", lines, [get_indent(indent_count) + "}"])
     return '\n'.join(result)
 
 
-def get_indent(type, indent_count):
+def get_indent(indent_count, type='unchanged'):
     if type == 'changed':
-        return ' ' * (indent_count - 2) + '- ', ' ' * (indent_count - 2) + '+ '
-    if type == 'unchanged' or type == 'has_children':
+        return (' ' * (indent_count - 2) + '- ',
+                ' ' * (indent_count - 2) + '+ ')
+    if type in ('unchanged', 'has_children'):
         return ' ' * indent_count
     elif type == 'deleted':
         return ' ' * (indent_count - 2) + '- '
@@ -27,21 +28,21 @@ def get_indent(type, indent_count):
         return ' ' * (indent_count - 2) + '+ '
 
 
-def check_value(tree, indent_count):
+def check_value(child, indent_count):
     result = []
-    type = tree.get('type')
-    key = tree.get('key')
-    value = tree.get('value')
-    indent = get_indent(type, indent_count)
+    type = child.get('type')
+    key = child.get('key')
+    value = child.get('value')
+    indent = get_indent(indent_count, type)
     if type == 'has_children':
-        result.append(f'{indent}{key}: {stylish(tree, indent_count)}')
+        result.append(f'{indent}{key}: {stylish(child, indent_count)}')
     if type in ('added', 'deleted', 'unchanged'):
         result.append(
             f'{indent}{key}: {stringify(value, indent_count)}'
         )
     if type == 'changed':
-        indent1, indent2 = get_indent(type, indent_count)
-        value1, value2 = tree.get('value1'), tree.get('value2')
+        indent1, indent2 = get_indent(indent_count, type)
+        value1, value2 = child.get('value1'), child.get('value2')
         result.append(f'{indent1}{key}: {stringify(value1, indent_count)}')
         result.append(f'{indent2}{key}: {stringify(value2, indent_count)}')
     return '\n'.join(result)
@@ -51,5 +52,5 @@ def stylish(tree, indent_count=0):
     lines = []
     for child in tree['children']:
         lines.append(check_value(child, indent_count + 4))
-    result = itertools.chain("{", lines, [indent_count * " " + "}"])
+    result = itertools.chain("{", lines, [get_indent(indent_count) + "}"])
     return '\n'.join(result)
